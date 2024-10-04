7d6f1d14f138d575843c91f9f3c4b4aa56d6962f
Alan Adamson <151865658+Agauna13@users.noreply.github.com>
2024-10-04 16:38:03 +0200

    ESlint configurado, capturas de pantalla en la carpeta media

---
+++ DWEC_AlanAdamson/configEntorno/test.js
@@ -0,0 +1,6 @@
+// Este código debería generar advertencias
+var name = "Alan";  // ESLint debería advertir por usar 'var'
+
+if (name == "Alan") {  // ESLint debería advertir por usar '=='
+    console.log("Hola, Alan");  // ESLint debería advertir por usar 'console.log'
+}
