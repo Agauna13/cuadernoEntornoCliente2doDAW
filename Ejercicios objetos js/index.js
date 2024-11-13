7d95b133647e53b354cfaa997566ae3d41fe98b7
Alan Adamson <151865658+Agauna13@users.noreply.github.com>
2024-11-13 15:53:42 +0100

    ...

--- Ejercicios objetos js/index.js
+++ Ejercicios objetos js/index.js
@@ -1,10 +1,3 @@
-/*1. Temporizador con alerta
-
-Crear una función llamada temporizadorAlerta que tome un número entero segundos como argumento.
-La función debe mostrar una alerta con el mensaje "Tiempo completado" después de los segundos especificados.
-Mientras el temporizador esté corriendo, el título de la página (usando document.title) debe actualizarse cada segundo con el tiempo restante en formato Restan: X segundos. Al completarse el temporizador, restablecer el título original de la página.*/
-
-
 function countdown(event) {
     event.preventDefault();
     let s = document.getElementById("segundos").value;
